82b48d4d96e7b87e0328a223aadb045f8e675272
Bruno Lavigne <brunolavigne@hotmail.com>
2015-12-12 22:17:24 -0500

    v1 du Web version avec utilisation du DLL

---
+++ Web_LocationVoitures/Web_LocationVoitures/Web/Index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lib_LocationVoiture;
+using LocationVoiture.Controller;
+using Lib_LocationVoiture.Model;
+
+namespace Web_LocationVoitures.Web
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        LocationController controlleur { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            controlleur = new LocationController();
+
+            List<client> clients = new List<client>();
+
+            clients = controlleur.ClientsServices.GetAllClients();
+
+            GridView1.DataSource = clients;
+            GridView1.DataBind();
+        }
+    }
+}
